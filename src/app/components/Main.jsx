2258ac38d4169be08410aa9f370cbe8b9b092b5e
Alexander Edemsky <qapple@ya.ru>
2017-08-18 16:11:17 +0300

    design changes

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -183,9 +183,8 @@ class Main extends React.Component {
   render() {
     return (
       <div>
-        <h1> Drag and drop videos </h1>
         <div className="input-src">
-          <p> Paste src link to video: </p>
+          <p> Paste video URL: </p>
           <input type="text" ref={(c) => { this.srcInput = c; }} />
           <button onClick={() => this.addVideo(this.srcInput.value)} >Add video</button>
         </div>
